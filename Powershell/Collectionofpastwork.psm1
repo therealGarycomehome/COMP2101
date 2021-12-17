20d863f13a340b4d22dadc92fb80b10ce70c783f
therealGarycomehome <robert.coyne1302@gmail.com>
2021-12-16 22:23:29 -0500

    module file upload

---
+++ Powershell/Collectionofpastwork.psm1
@@ -0,0 +1,5 @@
+﻿function welcome { "Welcome to powershell! Have a Great day."}
+
+function get-cpuinfo {get-ciminstance cim_processor | fl Manufacturer, Name, MaxClockSpeed, CurrentClockSpeed, NumberOfCores}
+
+function get-mydisks {Get-Disk | ft Manufacturer, Model, SerialNumber, FirmwareVersion, Size}
